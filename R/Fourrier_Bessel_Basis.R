56a12f884df7019567a19b69756053725fcbbbf7
Ismaila <ba.ismaila@courrier.uqam.ca>
2020-05-13 04:20:53 -0400

    Adding more pairwise interaction functions

--- R/Fourrier_Bessel_Basis.R
+++ R/Fourrier_Bessel_Basis.R
@@ -66,25 +66,87 @@ par(mfrow = c(1,1))
 
 ###############################################################################################
 ############### Fourier-Bessel basis
-## Example: Soft core point process
-softcore.param <- list(cif="sftcr", par=list(beta=Nbre_points/area(window), kappa=0.1, sigma=0.1), w=window)
+
+## Example 2: Soft core point process
+softcore.param <- list(cif="sftcr", par=list(beta=Nbre_points/area(window), kappa=0.2, sigma=0.01), w=window)
 softcore.mod <- rmh(softcore.param, start=list(n.start=Nbre_points), control=list(nrep=1e6), verbose=FALSE)
-Sk_ux <- Create.basis(softcore.mod, Y, Rmax = 0.10, rmin = 0.0001, 20, xrange = c(0,2), yrange = c(0,2))
-theta.est <- ppm(softcore.mod ~ ., covariates = Sk_ux)
-theta.true <- ppm(softcore.mod ~ 1, Strauss(r = 0.15))
+Sk_ux.softcore <- Create.basis(softcore.mod, Y, Rmax = 0.10, rmin = 0.0001, 50, xrange = c(0,2), yrange = c(0,2))
+theta.est.softcore <- ppm(softcore.mod ~ ., covariates = Sk_ux.softcore)
+theta.true.softcore <- ppm(softcore.mod ~ 1, Softcore(kappa=0.2,sigma=0.01), rbord=0.15)
 par(mfrow = c(1,2))
-plot(predict(theta.est, Y, type = "cif")); plot(predict(theta.true, Y, type ="cif"))
+plot(predict(theta.est.softcore, Y, type = "cif")); plot(predict(theta.true.softcore, Y, type ="cif"))
 par(mfrow = c(1,1))
 
+## Example 3: Diggle-Gates-Stibbard point process
+dgs.param <- list(cif="dgs", par=list(beta=Nbre_points/area(window), rho=0.01), w=window)
+dgs.mod <- rmh(dgs.param, start=list(n.start=Nbre_points), control=list(nrep=1e6), verbose=FALSE) 
+Sk_ux.dgs <- Create.basis(dgs.mod, Y, Rmax = 0.10, rmin = 0.0001, 50, xrange = c(0,2), yrange = c(0,2))
+theta.est.dgs <- ppm(dgs.mod ~ ., covariates = Sk_ux.dgs)
+theta.true.dgs <- ppm(dgs.mod ~ 1, DiggleGatesStibbard(rho=0.01), rbord=0.15)
+par(mfrow = c(1,2))
+plot(predict(theta.est.dgs, Y, type = "cif")); plot(predict(theta.true.dgs, Y, type ="cif"))
+par(mfrow = c(1,1))
 
+####################################################################################################
 
+####------- Pairwise interaction functions
 
-## Soft core interaction, 0<kappa<1 and sigma>0
+## 1. Hard-core, Strauss, and hybrid Strauss-hard core processes
+StrHc.pr <- function(t, hc=1, gamma=0.5, R=2, inter='hardcore') {
+  T <- length(t); e.t <- rep(NA, T)
+  for(i in 1:T) {
+    e.t[i] <- switch(inter,
+                     'hardcore' = { ifelse(t[i]>hc, 1, 0) },
+                     'strauss' = { ifelse(t[i]<=R, gamma, 1) },
+                     'strhc' = { ifelse(t[i]<=hc, 0, ifelse(((hc<t[i])&(t[i]<=R)), gamma, 1)) }
+    )}
+  return(e.t)
+}
+
+curve(StrHc.pr(x), 0, 3) 
+curve(StrHc.pr(x, inter='strauss'), 0, 3)
+curve(StrHc.pr(x, inter='strhc'), 0, 3)
+
+## 2. Diggle-Gratton potential
+DG.int <- function(t, rho, delta, kappa) {
+  T <- length(t); e.t <- rep(NA,T)
+  for(i in 1:T){
+    e.t[i] <- ifelse(t[i]<delta, 0, ifelse(((delta<=t[i])&(t[i]<rho)), ((t[i]-delta)/(rho-delta))^kappa, 
+                                           1))
+  }
+  return(e.t)
+}
+curve(DG.int(x,rho=3,delta=1,kappa=2), 0, 5)
+
+## 3. Diggle-Gates-Stibbard potential
+DGS.int <- function(t, rho=0.01) {
+  T <- length(t); e.t <- rep(NA,T)
+  for(i in 1:T) {
+    if(t[i]<rho) { e.t[i] <- (sin(pi*t[i]/(2*rho)))^2 }
+    else { e.t[i] <- 1 }
+  }
+  return(e.t)
+}
+curve(DGS.int(x,rho=1), 0, 2)
+
+## 4. Soft core interaction, 0<kappa<1 and sigma>0
 Soft.int <- function(r, kappa=0.1, sigma=0.1){
   return( exp( -(sigma/r)^(2/kappa) ) )
 }
-curve(Soft.int(x), 0, 5)
+curve(Soft.int(x,sigma=1), 0, 5)
 abline(h=1,lty="dashed")
+
+## 5. Fiksel potential
+Fik.int <- function(t, hc, r, kappa, a) {
+  T <- length(t); e.t <- rep(NA,T)
+  for(i in 1:T) {
+    e.t[i] <- ifelse(t[i]<hc, 0, ifelse(((hc<=t[i])&(t[i]<r)), exp(a*exp(-kappa*t[i])), 1))  
+  }
+  return(e.t)
+}
+curve(Fik.int(x,hc=1,r=3,kappa=1,a=-2), 0, 5)
+
+####################################################################################################
 
 ## kth element of the Cosine basis
 kthcoeff.Cosine <- function(r, R, k) {
@@ -118,8 +180,8 @@ Phi.hat <- function(r, R, rmin=0.001) {
 }
 
 
-curve(Soft.int(x,sigma=.1), 0, 0.15, main="softcore interaction")
-curve(Phi.hat(x,R=0.15,rmin=0.0001), from=0, to=0.15, lty="dashed", add=TRUE)
+curve(Soft.int(x,sigma=.05,kappa=0.2), 0, 0.15, main="softcore interaction",ylim=c(-1,1))
+curve(Phi.hat(x,R=0.10,rmin=0.0001), from=0, to=0.15, lty="dashed", add=TRUE)
 
 
 
